f4ac5f357f011a132949b78e8c043e8d229cdb16
Lorenzo Figueroa Wlack <lorenzo.f.w12@gmail.com>
2024-03-07 18:16:42 -0300

    multimedia

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,24 @@ document.addEventListener("DOMContentLoaded", function() {
           var iframe = collapseElement.querySelector("iframe");
   
           if (iframe) {
-            // Change the src attribute of the iframe based on the collapse ID
+            // Create a new Video instance based on the iframe
+            var video = new Video(iframe.src);
+  
+            // Change the URL of the video based on the collapse ID
             switch (collapseId) {
               case "collapseOne":
-                iframe.src = "https://www.youtube.com/embed/MUSIC_VIDEO_ID";
+                video.cambiarURL("https://www.youtube.com/embed/MUSIC_VIDEO_ID");
                 break;
               case "collapseTwo":
-                iframe.src = "https://www.youtube.com/embed/MOVIE_VIDEO_ID";
+                video.cambiarURL("https://www.youtube.com/embed/MOVIE_VIDEO_ID");
                 break;
               case "collapseThree":
-                iframe.src = "https://www.youtube.com/embed/SERIES_VIDEO_ID";
+                video.cambiarURL("https://www.youtube.com/embed/SERIES_VIDEO_ID");
                 break;
             }
+  
+            // Update the iframe's src attribute with the new URL
+            iframe.src = video.getUrl();
           } else {
             console.error("Iframe not found within the collapse element.");
           }
@@ -34,3 +40,34 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+  
+  // Multimedia class (parent class)
+  class Multimedia {
+    constructor(url) {
+      this._url = url;
+    }
+  
+    setInicio(url) {
+      this._url = url;
+    }
+  
+    getUrl() {
+      return this._url;
+    }
+  
+    cambiarURL() {
+      console.log("Este método es para realizar un cambio en la URL del video");
+    }
+  }
+  
+  // Video class (child class)
+  class Video extends Multimedia {
+    constructor(url) {
+      super(url);
+    }
+  
+    cambiarURL(nuevaURL) {
+      this._url = nuevaURL;
+      console.log("La URL del video ha sido cambiada a: " + this._url);
+    }
+  }
